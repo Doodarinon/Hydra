8dabe6711976645e67751ba19f8a5e6ed11b73cf
Doodarinon <97727583+Doodarinon@users.noreply.github.com>
2022-02-07 11:29:31 +0100

    Camera script that hopefully works

--- Hydra Project Week Game/Assets/Assets/Scripts/Camera_Script.cs
+++ Hydra Project Week Game/Assets/Assets/Scripts/Camera_Script.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 public class Camera_Script : MonoBehaviour
 {
 
+    public Camera cam;
+    Vector3 cameraDirection;
+    public float cameraDistance;
+    Vector2 cameraDistanceMinMax = new Vector2(0.5f, 5f);
 
     void Start()
     {
-
+        cameraDirection = transform.localPosition.normalized;
+        cameraDistance = cameraDistanceMinMax.y;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector3 desiredCameraPosition = transform.TransformPoint(cameraDirection * 3);
+        RaycastHit hit;
+        if(Physics.Linecast(transform.position, desiredCameraPosition, out hit))
+        {
+            cameraDistance = Mathf.Clamp(hit.distance * 0.9f, cameraDistanceMinMax.x, cameraDistanceMinMax.y);
+        }
+        else
+        {
+            cameraDistance = cameraDistanceMinMax.y;
+        }
+        cam.transform.localPosition = cameraDirection * cameraDistance;
     }
 }
