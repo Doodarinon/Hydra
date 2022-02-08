0449c150d2ccf71733c92e4c4b470d2d4515ff4b
liamchip65 <97727848+liamchip65@users.noreply.github.com>
2022-02-08 10:58:39 +0100

    PlayerHealth and enemies that deal damage to players.

--- Hydra Project Week Game/Assets/Assets/Scripts/EnemyBaseScript.cs
+++ Hydra Project Week Game/Assets/Assets/Scripts/EnemyBaseScript.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class EnemyBaseScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public float EnemyHeatlh = 10;
-    public float Damage = 0;
+    public float damagePerHit = 0;
+    public float attackspeed = 5f;
 
+    private float timer;
 
     public Transform target;
     public LayerMask raycastLayers;
     private NavMeshAgent nav;
     public float SeeDistece = 5f;
+
+    public PlayerHealth playerHealth;
+
     void Start()
     {
         nav = GetComponent<NavMeshAgent>();
@@ -23,6 +29,9 @@ public class EnemyBaseScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        
+
         RaycastHit hit;
         // If nothing is between this and target
         if (!Physics.Linecast(transform.position, target.position, out hit, raycastLayers))
@@ -40,13 +49,31 @@ public class EnemyBaseScript : MonoBehaviour
         }
 
         dead();
+
+        
     }
     public void dead()
     {
+        //If Enemys health under 0 
         if(EnemyHeatlh <= 0)
         {
             Destroy(gameObject);
         }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            //while (collision.collider.CompareTag("Player"))
+            
+                //Debug.Log("yes");
+                timer -= Time.deltaTime;
+                if (timer <= 0)
+                {
+                    playerHealth.healthPlayer -= damagePerHit;
+                }
+            
+        }
+    }
 }
