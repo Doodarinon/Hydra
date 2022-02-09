380e89dfdaa3342126c010a63a284558e2f1cadf
KerberosAkaBeta <97730062+KerberosAkaBeta@users.noreply.github.com>
2022-02-09 09:20:02 +0100

    d

---
+++ Hydra Project Week Game/Assets/Assets/Scripts/Bunker_Script.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bunker_Script : MonoBehaviour
+{
+
+    public float hp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = 500;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0)
+        {
+
+        } 
+    }
+}
