c84449441a99c32ca78e52a9828a12861301a477
liamchip65 <97727848+liamchip65@users.noreply.github.com>
2022-02-09 13:08:42 +0100

    HealthGUI script

---
+++ Hydra Project Week Game/Assets/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+    }
+
+    public void SetMaxHealth()
+    {
+
+    }
+}
