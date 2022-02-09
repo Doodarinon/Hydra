c346aafe4892dc8dffc6aa1e19c39a110385a84d
Scatss <thea.vonbromsen@elev.ga.lbs.se>
2022-02-09 14:35:10 +0100

    addeduse

--- Hydra Project Week Game/Assets/Assets/Scripts/Inventory.cs
+++ Hydra Project Week Game/Assets/Assets/Scripts/Inventory.cs
@@ -8,10 +8,12 @@ public class Inventory
     public event EventHandler OnItemListChange;
     // Creates list to track items in inventory!
     private List<Item> itemList;
+    private Action<Item> useItemAction;
 
     // Gives the player their inventory.
-    public Inventory()
+    public Inventory(Action<Item> useItemAction)
     {
+        this.useItemAction = useItemAction;
         itemList = new List<Item>();
 
         AddItem(new Item { itemType = Item.ItemType.BaseBallBat, amount = 1 });
@@ -71,6 +73,12 @@ public class Inventory
             itemList.Remove(item);
         }
         OnItemListChange?.Invoke(this, EventArgs.Empty);
+    }
+
+    // Allows the player to use an item.
+    public void UseItem(Item item)
+    {
+        useItemAction(item);
     }
 
     public List<Item> GetItemList()
