5080dae185809be530e21a917c99b1ab3d5f0c15
Scatss <thea.vonbromsen@elev.ga.lbs.se>
2022-03-23 11:48:39 +0100

    destroy on death

--- Hydra Project Week Game/Assets/Assets/Scripts/PlayerHealth.cs
+++ Hydra Project Week Game/Assets/Assets/Scripts/PlayerHealth.cs
@@ -9,9 +9,19 @@ public class PlayerHealth : MonoBehaviour
     public float currentPlayerHealth;
     public HealthBar healthbar;
 
+    private bool isDead;
+
     private void Start()
     {
         currentPlayerHealth = maxPlayerHealth;
         healthbar.SetMaxHealth(maxPlayerHealth);
+    }
+    private void Update()
+    {
+        if (currentPlayerHealth <= 0)
+        {
+            Destroy(gameObject);
+            isDead = true;
+        }
     }
 }
